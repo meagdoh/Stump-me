1827851c5f2db6b481d95e733a16761bc08d3a6e
meagdoh <meagdoherty@gmail.com>
2017-06-19 20:28:10 -0400

    Add drop down list

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,54 @@ Post.destroy_all
 
 # create your "parent" objects as usual
 
-energy = Topic.create(subject_name:"Energy & the Environment", img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000022100234Small_0-e1439312264426-150x150.jpg")
-fishing = Topic.create(subject_name:"Fishing", img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/iStock_000046942574Small-150x150.jpg")
-healthcare = Topic.create(subject_name:"Healthcare", img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000022593710Small-150x150.jpg")
+energy = Topic.create(
+  subject_name:"Energy & the Environment",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000022100234Small_0-e1439312264426-150x150.jpg")
+fishing = Topic.create(
+  subject_name:"Fishing",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/iStock_000046942574Small-150x150.jpg")
+healthcare = Topic.create(
+  subject_name:"Healthcare",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000022593710Small-150x150.jpg")
+budget = Topic.create(
+  subject_name:"Budget",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/Budget-Fiscal-ResponsibilitySmall1-150x150.jpg")
+education = Topic.create(
+  subject_name:"Education",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/5581e75b69044.image_-150x150.jpg")
+equality = Topic.create(
+  subject_name:"Equality",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/IMG_0313-150x150.jpg")
+foreign_policy = Topic.create(
+  subject_name:"Foreign Policy",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/150325-Ashraf-Ghani-150x150.jpg")
+gun_violence = Topic.create(
+  subject_name:"Gun Violence Protection",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2016/06/IMG_6723.jpg")
+housing = Topic.create(
+  subject_name:"Housing",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000021985122Medium-e1440356197546-150x150.jpg")
+immigration = Topic.create(
+  subject_name:"Immigration",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/iStock_000009439890_XXXLarge-150x150.jpg")
+jobs = Topic.create(
+  subject_name:"Jobs and the Economy",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/Jobs-EconomySmall-e1439309830885-150x150.jpg")
+opioid_epidemic = Topic.create(
+  subject_name:"Opioid Epidemic",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2016/04/IMG_5361-1-150x150.jpg")
+seniors = Topic.create(
+  subject_name:"Seniors",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2015/08/SeniorsSmall-150x150.jpg")
+transportation = Topic.create(
+  subject_name:"Transportation and Infrastructure",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2016/08/IMG_0900.jpg")
+veterans = Topic.create(
+  subject_name:"Veterans",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/IMG_4893-150x150.jpg")
+women_rights = Topic.create(
+  subject_name:"Women's Rights",
+  img_url:"https://moulton.house.gov/wp-content/uploads/2014/08/IMG_6264-150x150.jpg")
 
 
 # now that the parent exists in the DB
